795b326a3d7b9fadcad1c4d6550fa13710b9572d
bolaman-xyz <pcfoulme22@gmail.com>
2026-06-26 00:54:11 -0500

    Add CORS + dashboard talks to Railway bot instead of webhook

--- src/server.js
+++ src/server.js
@@ -6,6 +6,14 @@ const { loadSettings, saveSettings, maskToken } = require('./settings');
 const app = express();
 const PORT = process.env.PORT || 3847;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
